31a099c2a5fcb567af4733e04c89b52d4b4cbf2a
Egorick1845 <proegorweb@gmail.com>
2026-07-09 10:18:42 +0300

    улучшение культа ратвара

--- Content.Server/_Mini/Ratvar/Righteous/RatvarDoorSystem.cs
+++ Content.Server/_Mini/Ratvar/Righteous/RatvarDoorSystem.cs
@@ -1,3 +1,4 @@
+using Content.Server.Mind;
 using Content.Shared.Doors;
 using Content.Shared.RPSX.DarkForces.Ratvar.Righteous.Roles;
 
@@ -5,6 +6,8 @@ namespace Content.Server.RPSX.DarkForces.Ratvar;
 
 public sealed class RatvarDoorSystem : EntitySystem
 {
+    [Dependency] private readonly MindSystem _mind = default!;
+
     public override void Initialize()
     {
         SubscribeLocalEvent<PinionDoorComponent, BeforeDoorOpenedEvent>(OnBeforeDoorOpen);
@@ -12,8 +15,19 @@ public sealed class RatvarDoorSystem : EntitySystem
 
     private void OnBeforeDoorOpen(EntityUid uid, PinionDoorComponent comp, BeforeDoorOpenedEvent args)
     {
-        if (args.User != null && !HasComp<RatvarRighteousComponent>(args.User.Value))
-            args.Cancel();
+        if (args.User == null || CanUsePinionDoor(args.User.Value))
+            return;
+
+        args.Cancel();
+    }
+
+    private bool CanUsePinionDoor(EntityUid user)
+    {
+        if (HasComp<RatvarRighteousComponent>(user))
+            return true;
+
+        return HasComp<RatvarMarauderShellComponent>(user)
+               && _mind.TryGetMind(user, out _, out _);
     }
 }
 
